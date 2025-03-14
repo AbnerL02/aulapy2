1a8670340755d8a2963216e7df4fd7c3e77a6325
AbnerL02 <abner020826@gmail.com>
2025-03-13 21:29:22 -0300

    a

---
+++ index.js
@@ -0,0 +1,15 @@
+async function fetchData() {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+        const data = await response.json();
+        
+        // Substituir os dados no HTML
+        document.getElementById('name').textContent = data.forms[0].name;
+        document.getElementById('email').textContent = data.base_experience;
+    } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+    }
+}
+
+// Chamar a função ao carregar a página
+fetchData();
